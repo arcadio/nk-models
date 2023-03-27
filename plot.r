c757666cd948cf8e15e2fdff56bdb6a4717ca087
Arcadio Rubio García <arc@well.ox.ac.uk>
2023-03-27 01:31:19 +0100

    Add custom color palettes

--- plot.r
+++ plot.r
@@ -5,18 +5,19 @@ theme_set(theme_classic(base_size=18) + theme(plot.title=element_text(hjust=0.5)
 
 mft <- function() function(x) format(100*x, digits=2)
 
-fldplot <- function(id, dat, xlab, lab, xlim=NULL) {
+fldplot <- function(id, dat, xlab, lab, xlim=NULL, col=c(180, 300)) {
     p <- ggplot(dat, aes(x=fld, y=exp, fill=after_stat(x < 1))) +
         stat_halfeye(alpha=0.5, .width=c(0.025, 0.975), linewidth=4) +
         geom_vline(xintercept=1, linetype="dashed", color="grey") +
         coord_cartesian(xlim=xlim) +
         scale_y_discrete(labels=lab) +
+        scale_fill_hue(h=col) +
         labs(x=bquote(paste("Fold change ", "(", .(xlab), ")")), y="") +
         guides(fill="none")
     ggsave(paste0("tmp/fold", id, ".pdf"), p)
 }
 
-dotplot <- function(id, dat, err, tit, lab, ylim=NULL) {
+dotplot <- function(id, dat, err, tit, lab, ylim=NULL, col=c(180, 300)) {
     p <- ggplot(data=dat, mapping=aes(x=type, y=kill)) +
         geom_line(aes(group=id), alpha=0.1) +
         geom_point(size=3, aes(color=type, shape=day), alpha=0.9) +
@@ -24,18 +25,25 @@ dotplot <- function(id, dat, err, tit, lab, ylim=NULL) {
         stat_halfeye(data=err, aes(fill=type), alpha=0.5, .width=c(0.025, 0.975), linewidth=4) +
         scale_x_discrete(labels=lab) +
         scale_y_continuous(labels=mft()) +
+        scale_fill_hue(h=col, aesthetics=c("color", "fill")) +
         coord_cartesian(ylim=ylim) +
         labs(x="", y="Specific killing (%)", title=tit) +
         guides(color="none", fill="none", shape=guide_legend(title="Day"))
     ggsave(paste0("tmp/dot", id, ".pdf"), p)
 }
 
+pl1 <- c(0, 60)
+pl2 <- c(120, 180)
+pl3 <- c(240, 300)
+lhl <- c(bquote(Prolif^high), bquote(Prolif^low))
+
 fldplot("2b",
         rbind(data.frame(fld=dba$fld, exp="2b(a)"),
               data.frame(fld=dbb$fld, exp="2b(b)")),
         bquote(Kill[Tconv] / Kill[Treg]),
         c(bquote(CD56^br), bquote(CD56^dim)),
-        c(0.25, 2))
+        c(0.25, 2),
+        pl1)
 
 fldplot("2c",
         rbind(data.frame(fld=dca$fld, exp="tconv"),
@@ -43,7 +51,8 @@ fldplot("2c",
               data.frame(fld=dcc$fld, exp="all")),
         bquote(Kill[3.5] / Kill[2.5]),
         c("Tconv", "Treg", bquote(atop(Tconv + "", Treg))),
-        c(0.25, 4))
+        c(0.25, 4),
+        pl2)
 
 fldplot("4",
         rbind(data.frame(fld=d4c$fld, exp="4c"),
@@ -55,9 +64,8 @@ fldplot("4",
           bquote(atop(CD56^br  + "", Treg)),
           bquote(atop(CD56^dim + "", Tconv)),
           bquote(atop(CD56^dim + "", Treg))),
-        c(0.25, 2))
-
-lhl <- c(bquote(Prolif^high), bquote(Prolif^low))
+        c(0.25, 2),
+        pl3)
 
 dotplot("2b_a",
         read("2b_a"),
@@ -65,7 +73,8 @@ dotplot("2b_a",
               data.frame(type="treg", kill=dba$gm2)),
         bquote(CD56^br),
         c("Tconv", "Treg"),
-        c(0, 0.5))
+        c(0, 0.5),
+        pl1)
 
 dotplot("2b_b",
         read("2b_b"),
@@ -73,14 +82,16 @@ dotplot("2b_b",
               data.frame(type="treg", kill=dbb$gm2)),
         bquote(CD56^dim),
         c("Tconv", "Treg"),
-        c(0, 0.5))
+        c(0, 0.5),
+        pl1)
 
 dotplot("2c",
         read("2c"),
         rbind(data.frame(type="tcnv", kill=dcc$gm1),
               data.frame(type="treg", kill=dcc$gm2)),
         "",
-        c("Tconv", "Treg"))
+        c("Tconv", "Treg"),
+        col=pl2)
 
 dotplot("4c",
         read("4c"),
@@ -88,7 +99,8 @@ dotplot("4c",
               data.frame(type="low", kill=d4c$gm2)),
         bquote(CD56^br+Tconv),
         lhl,
-        c(0, 0.5))
+        c(0, 0.5),
+        pl3)
 
 dotplot("4d",
         read("4d"),
@@ -96,7 +108,8 @@ dotplot("4d",
               data.frame(type="low", kill=d4d$gm2)),
         bquote(CD56^br+Treg),
         lhl,
-        c(0, 0.5))
+        c(0, 0.5),
+        pl3)
 
 dotplot("4e",
         read("4e"),
@@ -104,7 +117,8 @@ dotplot("4e",
               data.frame(type="low", kill=d4e$gm2)),
         bquote(CD56^dim+Tconv),
         lhl,
-        c(0, 0.5))
+        c(0, 0.5),
+        pl3)
 
 dotplot("4f",
         read("4f"),
@@ -112,4 +126,5 @@ dotplot("4f",
               data.frame(type="low", kill=d4f$gm2)),
         bquote(CD56^dim+Treg),
         lhl,
-        c(0, 0.5))
+        c(0, 0.5),
+        pl3)
