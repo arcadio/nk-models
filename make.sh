23c48d32f30972c639788b50d474d08bfc6a235b
Arcadio Rubio García <arcadio@pm.me>
2024-06-28 04:14:03 +0200

    Add data download step

--- make.sh
+++ make.sh
@@ -1,5 +1,9 @@
-# Create tmp directory for storing results and run all models.
+# Download data, create tmp directory for storing results and run all
+# statistical inference models.
 
+SRC='https://github.com/arcadio/nk-models/releases/latest/download/dat.tar.gz'
+
+curl -L $SRC | tar -xzf -
 mkdir -p tmp
 
 Rscript hypothesis.r
