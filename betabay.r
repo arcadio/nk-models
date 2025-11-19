d55ede79c4d2773138b8d651d27f92b616912bee
Arcadio Rubio García <arcadio@pm.me>
2025-11-19 05:21:07 +0100

    Remove redundant function

--- betabay.r
+++ betabay.r
@@ -6,24 +6,6 @@ source("data.r")
 
 options(mc.cores=parallel::detectCores())
 
-run <- function(dat, bas, prd=T, var="type", fml=kill~type+id) {
-    fit <- stan_betareg(fml, dat, link="logit", link.phi="log", iter=5e3, seed=42)
-    pst <- posterior_epred(fit)
-    gm1 <- rowMeans(pst[,dat[[var]]==bas])
-    gm2 <- rowMeans(pst[,dat[[var]]!=bas])
-    if(prd) {
-        dif <- rowMeans(pst[,dat[[var]]==bas] - pst[,dat[[var]]!=bas])
-        fld <- rowMeans(pst[,dat[[var]]==bas] / pst[,dat[[var]]!=bas])
-    } else {
-        dif <- rowMeans(pst[,dat[[var]]==bas]) - rowMeans(pst[,dat[[var]]!=bas])
-        fld <- rowMeans(pst[,dat[[var]]==bas]) / rowMeans(pst[,dat[[var]]!=bas])
-    }
-    dry <- summary(dif)
-    fry <- summary(fld)
-    fsr <- ifelse(dry[3] > 0, mean(dif < 0), mean(dif > 0))
-    list(fit=fit, dif=dif, gm1=gm1, gm2=gm2, fld=fld, fry=fry, fsr=fsr)
-}
-
 rnc <- function(dat, bas, var="type", fml=kill~type+id) {
     fit <- stan_betareg(fml, dat, link="logit", link.phi="log", iter=5e3, seed=42)
     pst <- posterior_epred(fit)
@@ -59,21 +41,22 @@ d2c <- read("2c")
 tcv <- d2c[d2c$type == "tcnv",]
 trg <- d2c[d2c$type == "treg",]
 
-dba <- run(read("2b_a"), "tcnv")
-dbb <- run(read("2b_b"), "tcnv")
+dba <- rnc(read("2b_a"), "tcnv")
+dbb <- rnc(read("2b_b"), "tcnv")
 
-dca <- run(tcv, 3.5, prd=F, var="day", fml=kill~day)
-dcb <- run(trg, 3.5, prd=F, var="day", fml=kill~day)
-dcc <- run(d2c, "tcnv")
+dca <- rnc(tcv, 3.5, var="day", fml=kill~day)
+dcb <- rnc(trg, 3.5, var="day", fml=kill~day)
+dcc <- rnc(d2c, "tcnv")
 
-d4c <- run(read("4c"), "high")
-d4d <- run(read("4d"), "high")
-d4e <- run(read("4e"), "high")
-d4f <- run(read("4f"), "high")
+d4c <- rnc(read("4c"), "high")
+d4d <- rnc(read("4d"), "high")
+d4e <- rnc(read("4e"), "high")
+d4f <- rnc(read("4f"), "high")
 
 res <- dfm(list(dba, dbb, dca, dcb, dcc, d4c, d4d, d4e, d4f))
 rownames(res) <- c("2b(a)", "2b(b)", "2c(tcnv)", "2c(treg)", "2c(all)",
                    "4c", "4d", "4e", "4f")
+
 write.csv(res, file="tmp/res.csv")
 
 source("plot.r")
